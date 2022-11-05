c863c76b3b9a351b85f043733e8c9e1cc838a0bf
jeronimoV3 <jvaldez277@gmail.com>
2022-11-05 12:40:55 -0700

    Radix, counting fix

--- Code/Radix2.h
+++ Code/Radix2.h
@@ -3,10 +3,10 @@
 #include <iostream>
 #include <chrono>
 using namespace std;
-int originalSize;
-void countingSort(int original[]){
+
+void countingSort(int original[], int size){
     int maxValue = original[0];
-    for(int i= 0; i < originalSize; ++i ){
+    for(int i= 0; i < size; ++i ){
         if (original[i] > maxValue){
             maxValue = original[i];
         }
@@ -15,7 +15,7 @@ void countingSort(int original[]){
     for (int i = 0; i <= maxValue; ++i){
         freq[i] = 0;
     }
-    for (int i = 0; i < originalSize; ++i){
+    for (int i = 0; i < size; ++i){
         freq[original[i]] += 1;
     }
     int summ[maxValue+1];
@@ -23,24 +23,24 @@ void countingSort(int original[]){
     for(int j = 1; j <= maxValue; ++j){
         summ[j] = summ[j-1] + freq[j];
     }
-    int output[originalSize];
-    for (int i = 0; i < originalSize; ++i){
+    int output[size];
+    for (int i = 0; i < size; ++i){
         output[i] = 0;
     }
  
-    for(int n = originalSize-1; n >= 0; n--){
+    for(int n = size-1; n >= 0; n--){
         output[summ[original[n]]-1] = original[n];
 
         summ[original[n]] -= 1;
     }
-    for (int i = 0; i < originalSize; ++i){
+    for (int i = 0; i < size; ++i){
         original[i] = output[i];
     }
 
 }
-void countingSort2(int original[], int div){
+void countingSort2(int original[], int div, int size){
     int maxValue = original[0];
-    for(int i= 0; i < originalSize; ++i ){
+    for(int i= 0; i < size; ++i ){
         if (original[i] > maxValue){
             maxValue = original[i];
         }
@@ -49,36 +49,36 @@ void countingSort2(int original[], int div){
     for (int i = 0; i < 10; ++i){
         freq[i] = 0;
     }
-    for (int i = 0; i < originalSize; ++i){
+    for (int i = 0; i < size; ++i){
         freq[(original[i]/div)%10] += 1;
     }
     for(int j = 1; j < 10; ++j){
         freq[j] = freq[j-1] + freq[j];
     }
-    int output[originalSize];
-    for (int i = 0; i < originalSize; ++i){
+    int output[size];
+    for (int i = 0; i < size; ++i){
         output[i] = 0;
     }
-    for(int n = originalSize-1; n >= 0; n--){
+    for(int n = size-1; n >= 0; n--){
         output[freq[(original[n]/div)%10]-1] = original[n];
 
         freq[(original[n]/div)%10]--;
     }
-    for (int i = 0; i < originalSize; ++i){
+    for (int i = 0; i < size; ++i){
         original[i] = output[i];
     }
 }
 void radixsort(int *original2 , int size){
-    originalSize = size;
+    size = size;
     int maxValue = original2[0];
-    for(int i= 0; i < originalSize; ++i ){
+    for(int i= 0; i < size; ++i ){
         if (original2[i] > maxValue){
             maxValue = original2[i];
         }
     }
 
     for(int div=1; maxValue/div > 0; div *= 10){
-        countingSort2(original2, div);
+        countingSort2(original2, div, size);
     }
 
 }
